e082e9a5f7f31feef731b9977d6c2539043029dd
Erik <andd3r5>
2021-06-23 00:31:17 +0200

    script to convert images to jpg

---
+++ dev/pgm_to_jpg.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import os, glob
+ 
+def batch_image(in_dir, out_dir):
+    if not os.path.exists(out_dir):
+        print(out_dir, 'is not existed.')
+        os.mkdir(out_dir)
+    
+    if not os.path.exists(in_dir):
+        print(in_dir, 'is not existed.')
+        return -1
+    count = 0
+    for files in glob.glob(in_dir+'/*'):
+        filepath, filename = os.path.split(files)
+        
+        out_file = filename[0:19] + '.jpg'
+        # print(filepath,',',filename, ',', out_file)
+        im = Image.open(files)
+        new_path = os.path.join(out_dir, out_file)
+        print(count, ',', new_path)
+        count = count + 1
+        im.save(os.path.join(out_dir, out_file))
+        
+ 
+if __name__=='__main__':
+    #call function for every image folder
+    for subdir, dirs, files in os.walk(rootdir):
+    	batch_image('.CroppedYale/'+subdir, './CroppedYale_jpg/subdir')

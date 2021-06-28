08db8bc9a6c52fad66810e01c9af6a5c41b5d5bf
Oriol Garrobe <oriol.garrobe@gmail.com>
2021-06-28 17:07:45 +0000

    MultiHeadAttention WIP

--- dev/vit_model.py
+++ dev/vit_model.py
@@ -3,21 +3,25 @@ class PatchEmbedding(nn.Module):
         super().__init__()
         self.patch_size = patch_size #16
         self.class_token = torch.randn(1,1,emb_size) #image representation
-        self.pos_embedding = torch.randn((img_size // patch_size) **2, emb_size) #positional embedding of patch
+        self.pos_embedding = torch.randn((img_size // patch_size) **2 + 1, emb_size) #positional embedding of patch
 
         self.projection = nn.Sequential(
             # Convolution Layer to each patch
-            nn.Conv2d(in_channels=in_channels, out_channels=emb_size, kernel_size=patch_size, stride=patch_size),
-            # [batch, embedding_size, height, width]
-            # Flatten the resulting images -> Multiply: Height*Width
-            Rearrange('b e (h) (w) -> b (h w) e'),
+            nn.Conv2d(in_channels=in_channels, out_channels=emb_size, kernel_size=patch_size, stride=patch_size)
         )
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.projection(x)
-        batch,_,_=x.shape
+
+        #Flatten images after Convolution
+        x = torch.flatten(x, start_dim=2, end_dim=3)
+        x = torch.transpose(x, 1, 2)
+
+        batch,_,_=x.shape # batch size
+
         # Concatenate Class Tensor to Projected Patches
         class_tensor = self.class_token.repeat(batch,1,1)
+        x = torch.cat([class_tensor, x], dim=1)
 
         # Add Positional Embedding to Projected Patches
         x += self.pos_embedding

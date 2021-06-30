f313e6071f01ea2701bae6865b606c43412194a8
Oriol Garrobe <oriol.garrobe@gmail.com>
2021-06-30 10:00:11 +0000

    MultiHeadAttention done

--- dev/vit_model.py
+++ dev/vit_model.py
@@ -3,7 +3,7 @@ class PatchEmbedding(nn.Module):
         super().__init__()
         self.patch_size = patch_size #16
         self.class_token = torch.randn(1,1,emb_size) #image representation
-        self.pos_embedding = torch.randn((img_size // patch_size) **2 + 1, emb_size) #positional embedding of patch
+        self.pos_embedding = torch.randn((img_size // patch_size)**2 + 1, emb_size) #positional embedding of patch
 
         self.projection = nn.Sequential(
             # Convolution Layer to each patch
@@ -27,3 +27,66 @@ class PatchEmbedding(nn.Module):
         x += self.pos_embedding
 
         return x
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+
+        self.qs = nn.Linear(emb_size, emb_size)
+        self.ks = nn.Linear(emb_size, emb_size)
+        self.vs = nn.Linear(emb_size, emb_size)
+
+        self.A_drop = nn.Dropout(dropout)
+        self.projection = nn.Linear(emb_size, emb_size)
+
+    def forward(self, x : Tensor, mask: Tensor = None) -> Tensor:
+        b,n,_ = x.shape #b: batch_size || n: input_size
+        d = int(self.emb_size / self.num_heads) # d: sequence_length
+
+        # Create 'num_heads' subsets of Query, Key and Values with shape-> (BATCH_SIZE:b, NUM_HEADS:h, SEQUENCE_LENGTH:qdk, EMBEDDING_SIZE:d )
+        Q = self.qs(x).reshape((b, self.num_heads, n, d))
+        K = self.ks(x).reshape((b, self.num_heads, n, d))
+        V = self.vs(x).reshape((b, self.num_heads, n, d))
+
+        # Basic Attention
+        attention = torch.einsum('bhqd, bhkd -> bhqk', Q, K) / np.sqrt(Q.shape[-1]) # Matrix product between Q and K and Scaling
+
+        if mask is not None:
+            attention = attention.masked_fill(mask[None], -np.inf) #masked_fill -> torch function
+
+        attention = torch.softmax(attention, dim=3)
+        attention = self.A_drop(attention) #attention vector
+
+        # output processing
+        output = torch.einsum('bhal, bhlv -> bhav ', attention, V) #use attention to scale the values
+        output = torch.transpose(output, 1, 2)
+        output= torch.flatten(output, start_dim=2, end_dim=3)
+        output = self.projection(output)
+
+        return attention, output
+
+
+class Residuals(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.ffnn = ffnn #Feed Forward Neural Network ???
+
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.ffnn(x, **kwargs)
+        x += res
+        return x
+
+
+# The feed foeward network with 2 linear layers with a GELU nonlinearity
+class MLP(nn.Sequential):
+    def __init__(self, emb_size: int, expansion: int = 4, dropout: float = 0.):
+        super().__init__(
+            nn.Linear(emb_size, expansion * emb_size),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(expansion * emb_size, emb_size),
+        )
